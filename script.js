70eab6bf412ebc675302069f0b6c98727b52c3e2
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2024-05-09 16:00:06 +0530

    added scoring and clicked feature

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ for(var i=1;i<=180;i++){
 
 document.querySelector("#pbtm").innerHTML=clutter;
 }
-makeBubble();
 
 var timer=10;
 
@@ -21,8 +20,32 @@ function runTimer(){
         }
         else{
             clearInterval(samay);
+            document.querySelector("#pbtm").innerHTML=`<h1>GAME OVER</h1>`;
         }
     },1000);
 }
 
+var hitRn;
+function getNewHit(){
+    hitRn= Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent=hitRn;
+}
+var sum=0;
+
+function Sumplus(){
+    sum+=10;
+    document.querySelector("#scoreVal").textContent=sum;
+}
+
+document.querySelector("#pbtm").addEventListener("click",function(details){
+    var clickedNum=Number(details.target.textContent);
+    if(hitRn==clickedNum){
+        Sumplus();
+        getNewHit();
+        makeBubble();
+    }
+})
+
+makeBubble();
 runTimer();
+getNewHit();
