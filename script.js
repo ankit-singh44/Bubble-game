af36bd75bdf38e7239f8ffb29099696ed20b5a40
Ankit Kumar Singh <70136925+ankit-singh44@users.noreply.github.com>
2024-05-09 14:58:30 +0530

    putting multiple bubble and handling timer

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var clutter="";
+function makeBubble(){
+    var clutter="";
 
 
 for(var i=1;i<=180;i++){
-    clutter+= `<div class="bubble">5</div>`;
+    var rn=Math.floor(Math.random()*10);
+    clutter+= `<div class="bubble">${rn}</div>`;
 }
 
 document.querySelector("#pbtm").innerHTML=clutter;
+}
+makeBubble();
 
+var timer=10;
+
+function runTimer(){
+    var samay=setInterval(function(){
+        if(timer>0){
+        timer--;
+        document.querySelector("#timerval").textContent=timer;
+        }
+        else{
+            clearInterval(samay);
+        }
+    },1000);
+}
+
+runTimer();
